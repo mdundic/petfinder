8bc13e677c008836a2b4d4996a10284bfc2b710e
Milica Dundic <41840534+mdundic@users.noreply.github.com>
2019-06-23 14:12:00 +0200

    Import bootstrap template (#4)

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,25 @@
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport">
+<meta name="keywords" content="">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Pronađi ljubimca</title>
+
+<!-- Favicons -->
+<link href="img/favicon.png" rel="icon">
+<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700"
+    rel="stylesheet">
+
+<!-- Bootstrap CSS File -->
+<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Libraries CSS Files -->
+<link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="lib/animate/animate.min.css" rel="stylesheet">
+
+<!-- Main Stylesheet File -->
+<link href="css/style.css" rel="stylesheet">
---
+++ resources/views/layout/partials/nav.blade.php
@@ -0,0 +1,8 @@
+<nav id="nav-menu-container">
+    <ul class="nav-menu">
+        <li class="menu-active"><a href="#hero">Početna</a></li>
+        <li><a href="#about">O nama</a></li>
+        <li><a href="#contact">Kontakt</a></li>
+        <li><a href="#contact">Uloguj se</a></li>
+    </ul>
+</nav>
