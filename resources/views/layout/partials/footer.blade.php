44682ccc26275f1d53d0983d04999e070bf08c63
Milica Dundic <milicadundic@gmail.com>
2019-06-23 14:01:09 +0200

    Import bootstrap template

---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,16 @@
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="copyright">
+            &copy; Copyright <strong>Pronađi ljubimca</strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            Designed by PetFinder</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layout/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header id="header">
+    <div class="container">
+
+        <div id="logo" class="pull-left">
+            <a href="#hero">
+                <img src="img/logo.png" alt="" title="" id="logo-img" />
+            </a>
+        </div>
+
+        @include('layout.partials.nav')
+
+    </div>
+</header>
