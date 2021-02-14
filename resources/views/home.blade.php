252e46fb9f6ac0c44ba30d23e569c6c982457411
Milica Dundic <mili@Milicas-MacBook-Pro.local>
2021-02-14 21:11:20 +0100

    Remove unused include

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,6 @@
 <main id="main">
     @include('layout.sections.hero')
 
-    @include('layout.sections.lost-pets'), [
-        'pet_colors' => $pet_colors,
-        'pet_sizes' => $pet_sizes,
-        'pet_types' => $pet_types,
-        'locations' => $locations
-    ])
-
-    @include('layout.sections.found-pets'), [
-        'pet_colors' => $pet_colors,
-        'pet_sizes' => $pet_sizes,
-        'pet_types' => $pet_types,
-        'locations' => $locations
-    ])
-
     @include('layout.sections.about')
     
     @include('layout.sections.contact')
