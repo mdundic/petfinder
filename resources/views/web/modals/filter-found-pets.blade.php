2013d4f02131da76b5d971a7782bc05515c3b390
Milica Dundic <mili@Milicas-MacBook-Pro.local>
2021-02-15 14:31:16 +0100

    Keep modals vertically centered

--- resources/views/web/modals/filter-found-pets.blade.php
+++ resources/views/web/modals/filter-found-pets.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="found-pet-modal" tabindex="-1" role="dialog" aria-labelledby="foundPetLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="foundPetLabel">@lang('dictionary.hero.modal_title')</h5>
